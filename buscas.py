99c043791d72f4ea9f046b9d31ad804e1660f16c
thiagocparazzi <thiagocparazzi@gmail.com>
2019-09-11 19:58:58 -0300

    v-1.1 : Busca em Profundidade

--- buscas.py
+++ buscas.py
@@ -1,20 +1,24 @@
-#criando lista para adicionar as cidades do arquivo
-cities = {} #poderia ser também cities = file.readLines()
-
 def readFile():
     #lendo um arquivo txt
     file = open('cidades.txt', 'r') #abre o arquivo e lê
     #lê cada linha do arquivo 
-    for line in file:
-        key, value = line.split()
-        key = key.replace(',', '')
-        
-        if key not in cities:  #se a chave não estiver em cidades, ele recebe um valor, caso contrário é adicionado um novo valor
-            cities[key] = [value]
-        else:
-            cities[key].append(value)
-
+    file_stuff = file.readlines()
     file.close()
+    return file_stuff
+
+#prepara os dados que entrarão no dicionário
+def dictionary_data(data):
+    d = {}
+    for n in range(0, len(data)):
+        key, value = data[n].split()
+        key = key.replace(',', '')
+        value = value.replace(';', '')
+        #se a chave não estiver em cidades, ele recebe um valor, caso contrário é adicionado um novo valor
+        if key not in d:
+            d[key] = [value]
+        else:
+            d[key].append(value)
+    return d
 
 #busca em largura (Breadth-First Search) entre cidade de origem e cidade de destino
 def bfs(graph, start, goal):
@@ -32,14 +36,51 @@ def bfs(graph, start, goal):
             return path
         #neighbours (vizinhos) recebe o grafo a partir do nó atual
         neighbours = graph[node]
-        #se um vizinho estvier no grafo de vizinhos
+        #se um vizinho estiver no grafo de vizinhos
         #cria-se uma rota - através do caminho e adiciona tal vizinho a rota
         #a fila adiciona a rota
         for neighbour in neighbours:
             route = list(path)
             route.append(neighbour)
             queue.append(route)
- 
-readFile()
+
+#busca em largura (Depth-First Search) entre cidade de origem e cidade de destino
+def dfs(graph, start, goal):
+    #a pilha inicia-se com a cidade de origem
+    stack = [(start, [start])]
+    #prepara a lista de nós visitados
+    visited = set()
+    #enquanto a pilha não estiver vazia, fará tal processo
+    while stack:
+        #node e path recebem o último valor da pilha
+        (node, path) = stack.pop()
+        #se o nó não estiver em visitado, será adicionado
+        if node not in visited:
+            if node == goal:
+                return path
+            visited.add(node)
+            #neighbours (vizinhos) recebe o grafo a partir do nó atual
+            neighbours = graph[node]
+            #os vizinhos do grafo atual serão adicionados a fila, junto com seu caminho e lista de vizinhos
+            for neighbour in neighbours:
+                stack.append((neighbour, path + [neighbour]))
+        
+#####---MAIN---#####
+cities = readFile()
+start = ''
+goal = ''
+
+if not cities:
+    print('This file is empty')
+else:
+    goal = cities.pop(len(cities)-1).split()
+    start = cities.pop(len(cities)-1).split()
+    goal = goal[0].replace(';', '')
+    start = start[0].replace(';', '')
+
+graph = dictionary_data(cities)
+bfs = bfs(graph, start, goal)
+dfs = dfs(graph, start, goal)
 #bfs(nome_do_grafo, cidade_origem, cidade_destino)
-print(bfs(cities, 'SBO', 'Campinas'))
+print('Rota de Largura: ', bfs)
+print('Rota de Profundidade: ', dfs)
