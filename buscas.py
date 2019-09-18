cbe7c6d2462d5328fd4c9c71ad18b1858a0c7eff
thiagocparazzi <thiagocparazzi@gmail.com>
2019-09-18 18:42:01 -0300

    v-1.4 : Busca Custo Uniforme - início

--- buscas.py
+++ buscas.py
@@ -1,6 +1,8 @@
+#Projeto da disciplina de Inteligência Artificial «PUC-Campinas»
+
 def readFile():
     #lendo um arquivo txt
-    file = open('letras.txt', 'r') #abre o arquivo e lê
+    file = open('letras-com-custo.txt', 'r') #abre o arquivo e lê
     #lê cada linha do arquivo 
     file_stuff = file.readlines()
     file.close()
@@ -9,33 +11,35 @@ def readFile():
 #prepara os dados que entrarão no dicionário
 def dictionary_data(data):
     d = {}
-    for n in range(0, len(data)):
-        key, value = data[n].split(',')
-        key = key.replace(',', '')
-        value = value.replace(';', '').replace('\n', '')
-        #se a chave não estiver em cidades, ele recebe um valor, caso contrário é adicionado um novo valor
-        if key not in d:
-            d[key] = [value]
-        else:
-            d[key].append(value)
-    return d
-
-#prepara os dados que entrarão no dicionário com custo
-'''def dictionary_data_cost(data):
-    d = {}
-    for n in range(0, len(data)):
-        key, value, distance = data[n].split()
+    for line in data:
+        key, value, distance = line.split(',')
         key = key.replace(',', '')
         value = value.replace(',', '')
         distance = distance.replace(';', '')
         #se a chave não estiver em cidades, ele recebe um valor, caso contrário é adicionado um novo valor
         if key not in d:
-            d[key] = {[value]: [distance]}
+            d[key] = [value, distance]
         else:
-            d[key].append(value, distance)
-    return d'''
+            d[key].append(value)
+            d[key].append(distance)
+    return d
 
-#busca em largura (Breadth-First Search) entre cidade de origem e cidade de destino
+def dictionary_data_cost(data):
+    d = {}
+    for line in data:
+        key, value, distance = line.split(',')
+        key = key.replace(',', '')
+        value = value.replace(',', '')
+        distance = int(distance.replace(';', '').replace('\n', ''))
+        #se a chave não estiver em cidades, ele recebe um valor, caso contrário é adicionado um novo valor
+        if key not in d:
+            d[key] = [{value: distance}]
+        else:
+            d[key].append({value: distance})
+            print(d[key])
+    return d
+
+#busca em largura (Breadth-First Search) entre cidade de origem e cidade de destino #FIFO
 def bfs(graph, start, goal):
     #uma fila que se inicia com a cidade de origem
     queue = []
@@ -60,7 +64,7 @@ def bfs(graph, start, goal):
                 route.append(neighbour)
                 queue.append(route)
 
-#busca em profundidade (Depth-First Search) entre cidade de origem e cidade de destino
+#busca em profundidade (Depth-First Search) entre cidade de origem e cidade de destino #LIFO
 def dfs(graph, start, goal):
     #uma pilha que se inicia com a cidade de origem
     stack = []
@@ -85,6 +89,39 @@ def dfs(graph, start, goal):
                 route.append(neighbour)
                 stack.append(route)
 
+#busca de custo uniforme (Uniform Cost Search), utilizando o menor caminho entre cidades
+def ucs(graph, start, goal):
+    #uma fila que se inicia com a cidade de origem
+    visited = []
+    queue = []
+    queue.append([start])
+    #enquanto a fila não estiver vazia, fará tal processo
+    while queue:
+        #pega o primeiro caminho (path) da fila
+        path = queue.pop(0)
+        print('Path: ', path)
+        #pega o último nó do caminho
+        node = path[-1]
+        if type(node) == dict:
+            node = list(node.items())[0]
+            print('Node -> ', node, 'Tipo - ', type(node))
+            visited.append(node[0][0])
+            print('Visited:', visited)
+        #caso o caminho tenha sido encontrado
+        if node == goal:
+            return visited
+        #neighbours (vizinhos) recebe o grafo a partir do nó atual
+        elif node in graph:
+            neighbours = graph[node]
+            print('Vizinhos -> ', neighbours)
+        #se um vizinho estiver no grafo de vizinhos
+        #cria-se uma rota - através do caminho e adiciona tal vizinho a rota
+        #a fila adiciona a rota
+            for neighbour in neighbours: 
+                route = list(visited)
+                route.append(neighbour)
+                queue.append(route)
+
         
 #####---MAIN---#####
 cities = readFile()
@@ -100,10 +137,12 @@ else:
     start = start[0].replace(';', '')
 
 graph = dictionary_data(cities)
-#graph_cost = dictionary_data_cost(cities)
+graph_cost = dictionary_data_cost(cities)
 
 bfs = bfs(graph, start, goal)
 dfs = dfs(graph, start, goal)
+ucs = ucs(graph_cost, start, goal)
 
 print('Rota de Largura: ', bfs)
 print('Rota de Profundidade: ', dfs)
+print('Rota de Custo Uniforme: ', ucs)
